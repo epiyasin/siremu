87cf6d8359b5f4a8c2324923bef063edf49bcb24
epiyasin <yasin__elmaci@hotmail.com>
2023-08-06 16:14:38 +0100

    introduce an adaptive learning rate shceduler

--- training.py
+++ training.py
@@ -1,7 +1,16 @@
 import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from tqdm import tqdm
 
-def train_model(model, criterion, optimizer, train_loader, val_loader, epochs):
+def train_model(model, criterion, optimizer, train_loader, val_loader, settings):
+    # Retrieve values from settings
+    epochs = settings["nn_epochs"]
+    step_size = settings["lr_scheduler"]["step_size"]
+    gamma = settings["lr_scheduler"]["gamma"]
+
+    # Define the learning rate scheduler
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+
     progress_bar = tqdm(range(epochs), desc='Training model', dynamic_ncols=True)
 
     for epoch in progress_bar:
@@ -11,6 +20,8 @@ def train_model(model, criterion, optimizer, train_loader, val_loader, epochs):
             loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
+        # Step the learning rate scheduler
+        scheduler.step()
 
         # Validation
         if val_loader:
@@ -22,4 +33,6 @@ def train_model(model, criterion, optimizer, train_loader, val_loader, epochs):
                     val_loss += criterion(outputs, targets).item()
                     val_batches += 1
                 avg_val_loss = val_loss / val_batches
-                progress_bar.set_description(f'Training model (Loss: {loss.item():.4f}, Avg Val Loss: {avg_val_loss:.4f}), completed')
+                
+                current_lr = optimizer.param_groups[0]['lr']
+                progress_bar.set_description(f'Training model (Loss: {loss.item():.2f}, Avg Val Loss: {avg_val_loss:.2f}, LR: {current_lr:.2e}), completed')
