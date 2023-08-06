087dc5a1a78d8ecb0c5091bc306180e8fee16a5c
epiyasin <yasin__elmaci@hotmail.com>
2023-08-06 16:16:34 +0100

    introduce parallelisation into ABM data generation pipeline

--- ABM.py
+++ ABM.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
 def update_agents(agents, infection_rate, recovery_rate, N):
     new_infections = 0
@@ -17,31 +18,32 @@ def update_agents(agents, infection_rate, recovery_rate, N):
     new_infections = (new_infections / N) * 1000
     return agents, new_infections, new_recoveries
 
+def generate_iteration(infection_rate, recovery_rate, settings):
+    agents = np.repeat('S', settings["population_size"])
+    agents[np.random.choice(settings["population_size"])] = 'I'
+    SIR_data = {'S': [], 'I': [], 'R': []}
+    incidences = []
+    for t in range(1, settings["num_time_steps"] + 1):
+        agents, new_infections, new_recoveries = update_agents(agents, infection_rate, recovery_rate, settings["population_size"])
+        SIR_data['S'].append(np.sum(agents == 'S'))
+        SIR_data['I'].append(np.sum(agents == 'I'))
+        SIR_data['R'].append(np.sum(agents == 'R'))
+        incidences.append(new_infections)
+    return {'SIR_data': pd.DataFrame(SIR_data), 'incidences': incidences}
+
 def generate_ABM_data(settings):
     all_realisations = []
-    
+
     realisation_progress = tqdm(range(settings["num_realisations"]), desc='Realisations', dynamic_ncols=True)
-    
+
     np.random.seed(settings["random_seed"])
 
     for _ in realisation_progress:
         infection_rate = np.random.uniform(*settings["infection_rate_range"])
         recovery_rate = np.random.uniform(*settings["recovery_rate_range"])
 
-        iteration_progress = tqdm(range(settings["num_iterations"]), desc=f'Iterations (Infection Rate: {infection_rate:.2f}, Recovery Rate: {recovery_rate:.2f})', leave=False, dynamic_ncols=True)
+        with ProcessPoolExecutor(max_workers=settings["max_workers"]) as executor:
+            future_results = [executor.submit(generate_iteration, infection_rate, recovery_rate, settings) for _ in range(settings["num_iterations"])]
+            all_realisations.extend([future.result() for future in future_results])
 
-        for _ in iteration_progress:
-            agents = np.repeat('S', settings["population_size"])
-            agents[np.random.choice(settings["population_size"])] = 'I'
-            SIR_data = {'S': [], 'I': [], 'R': []}
-            incidences = []
-            for t in range(1, settings["num_time_steps"] + 1):
-                agents, new_infections, new_recoveries = update_agents(agents, infection_rate, recovery_rate, settings["population_size"])
-                SIR_data['S'].append(np.sum(agents == 'S'))
-                SIR_data['I'].append(np.sum(agents == 'I'))
-                SIR_data['R'].append(np.sum(agents == 'R'))
-                incidences.append(new_infections)
-            all_realisations.append({'SIR_data': pd.DataFrame(SIR_data), 'incidences': incidences})
-            iteration_progress.set_description(f'Iterations (Infection Rate: {infection_rate:.2f}, Recovery Rate: {recovery_rate:.2f}), completed')
-        realisation_progress.set_description('Realisations, completed')
     return all_realisations
