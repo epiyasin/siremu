7b9829df8b8e20e714b49867cd96d5c0e0780231
epiyasin <yasin__elmaci@hotmail.com>
2023-08-08 14:52:47 +0100

    Create nn_data_handler.py

---
+++ nn_data_handler.py
@@ -0,0 +1,34 @@
+import torch
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import DataLoader, TensorDataset
+from config import settings
+
+def prepare_nn_data(X, Y):
+    # Split data into train, validation, and test
+    X_temp, X_test, Y_temp, Y_test = train_test_split(X, Y, test_size=settings["data"]["test_pct"], random_state=settings["execution"]["random_seed"])
+    X_train, X_val, Y_train, Y_val = train_test_split(X_temp, Y_temp, test_size=settings["data"]["val_pct"]/(1-settings["data"]["test_pct"]), random_state=settings["execution"]["random_seed"])
+
+    scaler = StandardScaler()
+    
+    # Normalize your input data
+    X_train_scaled = scaler.fit_transform(X_train.numpy())
+    X_val_scaled = scaler.transform(X_val.numpy())
+    X_test_scaled = scaler.transform(X_test.numpy())
+
+    # Convert normalized data back to tensors
+    X_train = torch.tensor(X_train_scaled, dtype=torch.float32)
+    X_val = torch.tensor(X_val_scaled, dtype=torch.float32)
+    X_test = torch.tensor(X_test_scaled, dtype=torch.float32)
+
+    # Convert to tensor datasets
+    train_data = TensorDataset(X_train, Y_train)
+    val_data = TensorDataset(X_val, Y_val)
+    test_data = TensorDataset(X_test, Y_test)
+
+    # Create data loaders
+    train_loader = DataLoader(train_data, batch_size=settings["neural_net"]["nn_batch_size"], shuffle=settings["data"]["shuffle"], num_workers=settings["data"]["num_workers"])
+    val_loader = DataLoader(val_data, batch_size=settings["neural_net"]["nn_batch_size"], shuffle=settings["data"]["shuffle"], num_workers=settings["data"]["num_workers"])
+    test_loader = DataLoader(test_data, batch_size=settings["neural_net"]["nn_batch_size"], shuffle=settings["data"]["shuffle"], num_workers=settings["data"]["num_workers"])
+
+    return train_loader, val_loader, test_loader, scaler
